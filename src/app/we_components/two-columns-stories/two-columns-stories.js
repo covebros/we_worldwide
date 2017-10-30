8080c2efd8333fb2597205eaf8012d3a7ca5f0bf
Krystian Kwiatkowski <k.kwiatkowski@ymail.com>
2017-10-30 15:22:41 +0000

    Stories page content added in

---
+++ src/app/we_components/two-columns-stories/two-columns-stories.js
@@ -0,0 +1,11 @@
+angular
+.module('app')
+.component('twoColumnsStories', {
+  templateUrl: 'app/we_components/two-columns-stories/two-columns-stories.html',
+  controller: twoColumnsStories
+});
+
+  /** @ngInject **/
+
+function twoColumnsStories($scope, $timeout) {
+}
