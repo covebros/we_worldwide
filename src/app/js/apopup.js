5c9a29ca5e2060ee14fcc7f3db0ac115e2dd5d1c
Krystian Kwiatkowski <k.kwiatkowski@ymail.com>
2017-11-05 14:14:18 +0000

    popup js added in and button

---
+++ src/app/js/apopup.js
@@ -0,0 +1,12 @@
+(function () {
+	'use strict';
+  	angular.element(function() {
+  		angular.element(".open-popup").fullScreenPopup({
+  			bgColor: "#fff",
+			inlineStyles: true,
+			animationSpeed: 200
+  		});
+  	});
+})();
+
+
--- src/app/pages/about/about.js
+++ src/app/pages/about/about.js
@@ -1,11 +1,19 @@
 (function () {
   'use strict';
 
-  angular
-  .module('app')
-  .controller('aboutController', aboutController);
+angular
+.module('app')
+.controller('aboutController', aboutController);
 
-  /** @ngInject */
-  function aboutController($scope, $timeout) {
-  }
+  	/** @ngInject */
+  	function aboutController($scope) {
+  		$scope.popup = function () {
+  			console.log('button clicked');
+  			angular.element(".open-popup").fullScreenPopup({
+				bgColor: "green",
+				inlineStyles: true,
+				animationSpeed: 200
+  		});
+  	}
+}
 })();
