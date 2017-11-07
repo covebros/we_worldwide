176ea46133332ab15bb62454ec7ffc71cda57557
Krystian Kwiatkowski <k.kwiatkowski@ymail.com>
2017-11-07 16:24:37 +0000

    Bootstrap modal added in

--- src/app/js/a-popup.js
+++ src/app/js/a-popup.js
@@ -1,13 +1,5 @@
 (function () {
 	'use strict';
-  	angular.element(function() {
-  		angular.element(".open-popup").fullScreenPopup({
-  			bgColor: "#fff",
-			inlineStyles: true,
-			animationSpeed: 200
-  		});
-  	});
-
   	angular.element(document).ready(function () {
     	objectFitVideos();
     });
--- src/app/pages/about/about.js
+++ src/app/pages/about/about.js
@@ -4,16 +4,7 @@
 angular
 .module('app')
 .controller('aboutController', aboutController);
-
   	/** @ngInject */
   	function aboutController($scope) {
-  		$scope.popup = function () {
-  			console.log('button clicked');
-  			angular.element(".open-popup").fullScreenPopup({
-				bgColor: "#5a5a5a",
-				inlineStyles: true,
-				animationSpeed: 200
-  		});
-  	}
-}
+  }
 })();
