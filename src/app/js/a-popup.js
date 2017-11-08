4e764095eaec97a07e44bb9519ff2f39c5ffad16
Krystian Kwiatkowski <k.kwiatkowski@ymail.com>
2017-11-08 20:29:41 +0000

    Final changes applied

--- src/app/js/a-popup.js
+++ src/app/js/a-popup.js
@@ -3,12 +3,6 @@
   	angular.element(document).ready(function () {
     	objectFitVideos();
     });
-    angular.element(document).ready(function () {
-    	if (angular.element(window).width() < 992) {
-        	angular.element('.mobile-remove').removeClass('animated');
-        	angular.element('.mobile-remove').removeAttr('style');
-      	}
-    });
 })();
 
 
